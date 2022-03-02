fd464dc3cc2b4e435857fef8f9a6006eef8f6070
Victor Sarkisov <100457265+octalberrys@users.noreply.github.com>
2022-03-02 02:06:01 +0000

    Deadfish backend

---
+++ src/deadfish.c
@@ -0,0 +1,11 @@
+#include "esolangs.h"
+
+int df_init(char *src)
+{
+
+}
+
+int df_reset(void)
+{
+    
+}
---
+++ src/esolangs.h
@@ -0,0 +1,7 @@
+#ifndef _ESOLANGS_H_
+#define _ESOLANGS_H_
+
+int df_init(char *src);
+int df_reset(void);
+
+#endif
