9b5212821d6e8b793c219862e8a1b47d6647e3d6
Victor Sarkisov <100457265+octalberrys@users.noreply.github.com>
2022-03-02 23:41:40 -0500

    Finished brainfuck implementation

--- src/brainfuck.c
+++ src/brainfuck.c
@@ -1,27 +1,96 @@
 #include "esolangs.h"
 
-static unsigned char *tape = 0;
-static unsigned long ptr = 0;
-
-static void grow_tape()
-{
-    tape = realloc(tape, sizeof(tape) + BF_TAPE_SIZE);
-}
-
 int brainfuck_exec(char *src)
 {
-    if (!tape) {brainfuck_reset();}
+    size_t tptr = 0;
+    size_t tmult = 1;
 
-    for (int i = 0; i < strlen(src); i++)
+    unsigned char *tape = malloc(sizeof(unsigned char) * tmult * BF_TAPE_SIZE);
+    memset(tape, 0, sizeof(unsigned char) * tmult * BF_TAPE_SIZE);
+
+    for (size_t pc = 0;src[pc] != 0;pc++)
     {
-        unsigned char ins = src[i];
+        unsigned char ins = src[pc];
+
+        switch (ins)
+        {
+            case '>':
+                if (tptr == tmult * BF_TAPE_SIZE)
+                {
+                    tmult ++;
+                    tape = realloc(tape, sizeof(unsigned char) * tmult * BF_TAPE_SIZE);
+                }
+
+                tptr ++;
+                break;
+            
+            case '<':
+                if (tptr > 0) tptr --;
+                break;
+            
+            case '+':
+                tape[tptr] ++;
+                break;
+            
+            case '-':
+                tape[tptr] --;
+                break;
+                
+            case '.':
+                printf("%c", tape[tptr]);
+                break;
+
+            case ',':
+                //get user input
+                break;
+
+            case '[':
+                if (tape[tptr] == 0)
+                {
+                    unsigned long count = 0;
+
+                    while (1)
+                    {
+                        pc++;
+
+                        if (!src[pc]) break;
+                        if (src[pc] == '[') count++;
+
+                        else if (src[pc] == ']') 
+                        {
+                            if (count) count--;
+                            else break;
+                        }
+                    }
+                }
+
+                break;
+            
+            case ']':
+                if (tape[tptr] != 0)
+                {
+                    unsigned long count = 0;
+
+                    while (1)
+                    {
+                        pc--;
+
+                        if (!pc) break;
+                        if (src[pc] == ']') count++;
+
+                        else if (src[pc] == '[') 
+                        {
+                            if (count) count--;
+                            else break;
+                        }
+                    }
+                }
+                break;
+        }
+
+        if (tape[tptr] > 255) tape[tptr] -= 256;
+        if (tape[tptr] < 0) tape[tptr] += 256;
     }
 
     return 0;
-}
-
-void brainfuck_reset(void)
-{
-    free(tape);
-    tape = malloc(sizeof(char) * BF_TAPE_SIZE);
 }
