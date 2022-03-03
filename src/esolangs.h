b9e44401b9773abe90557375ba3a3954f89bd4a6
Victor Sarkisov <100457265+octalberrys@users.noreply.github.com>
2022-03-03 00:30:43 -0500

    Removed unused constants

--- src/esolangs.h
+++ src/esolangs.h
@@ -5,12 +5,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define ERR_INVALID_TOKEN 0x1
-
 #define BF_TAPE_SIZE  30000
-#define BF_STACK_SIZE   500
-#define BF_MODE_INS       1
-#define BF_MODE_BR        2
 
 typedef enum lang_t
 {
